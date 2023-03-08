525f3d3c2e994f44b816d632d0ee0a146d08dc5e
tavopaz <tavo.paz12.dev@hotmail.com>
2023-03-08 12:40:58 -0600

    chage url socket

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { transports: ["websocket"] });
-app.use(cors());
 
 let users = [];
 
