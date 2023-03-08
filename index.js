73b06a43dc6c8437fbdb197fb09f97753fb5535c
tavopaz <tavo.paz12.dev@hotmail.com>
2023-03-07 21:33:10 -0600

    new commit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+app.use(cors());
 
 let users = [];
 
